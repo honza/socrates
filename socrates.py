37ade5f4ce1feb44cdb7f8de1e373f5085c77a40
Honza Pokorny <me@honza.ca>
2011-02-23 22:03:19 -0400

    Add simple server for testing purposes.

--- socrates.py
+++ socrates.py
@@ -6,6 +6,7 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option('-i', '--init', action='store_true', help='Some help')
 parser.add_option('-g', '--generate', action='store_true', help='Some help')
+parser.add_option('-r', '--run', action='store_true', help='Some help')
 
 options, args = parser.parse_args()
 
@@ -14,3 +15,20 @@ if options.init:
 
 if options.generate:
     main()
+
+if options.run:
+    import SimpleHTTPServer
+    import SocketServer
+    import os
+
+    os.chdir('blog/deploy')
+
+    PORT = 8000
+
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+    print "serving at port", PORT
+    httpd.serve_forever()
+        
