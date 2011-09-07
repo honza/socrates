9ed685c8d62c41827809976f1ab24e1cc3367caa
Honza Pokorny <me@honza.ca>
2011-09-07 13:09:46 -0700

    Fix packaging.

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ long_desc = open('README.rst').read()
 
 setup(
     name='socrates',
-    version='0.6.1',
+    version='0.6.4',
+    url='http://honza.github.com/socrates/',
     install_requires=['pyYAML', 'markdown'],
     description=description,
     long_description=long_desc,
     author='Honza Pokorny',
+    author_email='me@honza.ca',
     maintainer='Honza Pokorny',
     maintainer_email='me@honza.ca',
     packages=['socrates'],
