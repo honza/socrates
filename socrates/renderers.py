ef28e25526c251059b6efad0203fcb3e8ff66d3b
Honza Pokorny <me@honza.ca>
2012-12-19 21:02:25 -0400

    Add in_category template filter

--- socrates/renderers.py
+++ socrates/renderers.py
@@ -27,7 +27,13 @@ class Jinja2Renderer(BaseRenderer):
     def __init__(self, path):
         from jinja2 import Environment, FileSystemLoader
         self.env = Environment(loader=FileSystemLoader(path))
+        self.env.filters['in_category'] = self.is_in_category
 
     def render(self, template, values):
         t = self.env.get_template(template)
         return t.render(values)
+
+    @classmethod
+    def is_in_category(cls, post, category):
+        slugs = [c['slug'] for c in post.categories]
+        return category in slugs
