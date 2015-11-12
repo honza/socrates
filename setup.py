354d9a3239b5d89541e105a9882eeb2fb985f3ee
Honza Pokorny <me@honza.ca>
2015-11-12 11:22:23 -0400

    Pin dependencies

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ long_desc = open('README.rst').read()
 
 setup(
     name='socrates',
-    version='0.9.0',
+    version='0.9.1',
     url='http://honza.github.com/socrates/',
     install_requires=[
-        'pyYAML',
-        'misaka',
-        'django',
-        'jinja2',
-        'docutils',
-        'pygments',
-        'textile'
+        'PyYAML==3.11',
+        'misaka==2.0.0',
+        'Django==1.8.6',
+        'Jinja2==2.8',
+        'docutils==0.12',
+        'Pygments==2.0.2',
+        'textile==2.2.2'
     ],
     description=description,
     long_description=long_desc,
